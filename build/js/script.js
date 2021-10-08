917984f15faec10d5e53b49164deddaba4edf1a6
Юрий <turbofan2000@mail.ru>
2021-10-09 02:20:05 +0300

    Навигация не полностью

--- build/js/script.js
+++ build/js/script.js
@@ -1,4 +1,22 @@
 'use strict';
+
+//menu
+
+var navMain = document.querySelector(".main-nav");
+var navToggle = document.querySelector(".main-nav__toggle");
+
+navMain.classList.remove("main-nav--nojs");
+
+navToggle.addEventListener("click", function() {
+  if (navMain.classList.contains("main-nav--closed")) {
+    navMain.classList.remove("main-nav--closed");
+    navMain.classList.add("main-nav--opened");
+  } else {
+    navMain.classList.add("main-nav--closed");
+    navMain.classList.remove("main-nav--opened");
+  }
+});
+/*
 var pageHeader = document.querySelector('.page-header');
 var headerToggle = document.querySelector('.page-header__toggle');
 
@@ -13,3 +31,4 @@ headerToggle.addEventListener('click', function () {
     pageHeader.classList.remove('page-header--opened');
   }
 });
+*/
