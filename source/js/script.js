f8c12932a4a4321967e2b05c6477e9780f72c8b1
Юрий <turbofan2000@mail.ru>
2021-10-12 02:15:00 +0300

    Блок виды велосипедов

--- source/js/script.js
+++ source/js/script.js
@@ -1,7 +1,6 @@
 'use strict';
 
-//menu
-
+//основное меню
 var navMain = document.querySelector(".main-nav");
 var navToggle = document.querySelector(".main-nav__toggle");
 
@@ -16,19 +15,3 @@ navToggle.addEventListener("click", function() {
     navMain.classList.remove("main-nav--opened");
   }
 });
-/*
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
-
-pageHeader.classList.remove('page-header--nojs');
-
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
-  }
-});
-*/
