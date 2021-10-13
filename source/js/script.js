4b7c23b0d6c82276570f412b7f4a9808fe4c948d
Юрий <turbofan2000@mail.ru>
2021-10-14 02:20:31 +0300

    окончательно

--- source/js/script.js
+++ source/js/script.js
@@ -15,3 +15,35 @@ navToggle.addEventListener("click", function() {
     navMain.classList.remove("main-nav--opened");
   }
 });
+
+//плавный скролл к якорям
+const anchors = document.querySelectorAll("a[href*=\"#\"]")
+
+for (let anchor of anchors) {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault()
+
+    const blockID = anchor.getAttribute("href").substr(1)
+
+    document.getElementById(blockID).scrollIntoView({
+      behavior: "smooth",
+      block: "start"
+    })
+  })
+}
+
+//запись полей формы в Local Storage
+var blockForm = document.querySelector(".slogan__wrapper-form");
+var userName = blockForm.querySelector("[name=user-name]");
+var userPhone = blockForm.querySelector("[name=user-telephone]");
+var form = blockForm.querySelector("form");
+
+form.addEventListener("submit", function (evt) {
+  if (!userName.value || !userPhone.value) {
+    evt.preventDefault();
+    console.log("Нужно заполнить все поля формы");
+  } else {
+    localStorage.setItem("userName", userName.value);
+    localStorage.setItem("userPhone", userPhone.value);
+  }
+});
